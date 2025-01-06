3970fe6b009fda45e9a755c1e1e908d71149832a
userbyte <42632711+userbyte@users.noreply.github.com>
2025-01-06 18:08:32 -0500

    build standalone

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const json = require("./package.json");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   publicRuntimeConfig: {
     version: json.version,
   },

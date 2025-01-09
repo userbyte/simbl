02f5a4b0f8186fad4388ae73f05eaaef5ad5fb0e
userbyte <42632711+userbyte@users.noreply.github.com>
2025-01-08 19:58:15 -0500

    fix images in metadata component

--- src/app/components/Metadata.tsx
+++ src/app/components/Metadata.tsx
@@ -8,14 +8,14 @@ export default function Metadata() {
       <meta property="og:title" content="simbl" />
       <meta
         property="og:image"
-        content="https://userbyte.xyz/simbl/img/simbl.png"
+        content="https://simbl.userbyte.xyz/img/png/logo.png"
       />
       <meta property="og:description" content="a dumb little microblog" />
-      <link rel="manifest" href="pwa/manifest.json" />
-      <link rel="apple-touch-icon" href="img/svg/logo.svg" />
+      <link rel="manifest" href="/pwa/manifest.json" />
+      <link rel="apple-touch-icon" href="/img/svg/logo.svg" />
       <meta name="theme-color" content="#000000" />
       <meta name="viewport" content="user-scalable=no" />
-      <link rel="icon" href="img/svg/logo.svg" />
+      <link rel="icon" href="/img/svg/logo.svg" />
       <title>simbl</title>
     </head>
   );

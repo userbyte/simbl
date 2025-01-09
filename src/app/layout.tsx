95e90048dc105b2fa740f975a6f3032afdb7be20
userbyte <42632711+userbyte@users.noreply.github.com>
2025-01-08 20:00:12 -0500

    metadata rework

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,33 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./globals.css";
-import Metadata from "./components/Metadata";
+import { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "simbl",
+  description: "a dumb little microblog",
+  keywords: ["microblog", "twitter clone"],
+  manifest: "/pwa/manifest.json",
+  icons: {
+    icon: "/img/png/logo.png",
+    shortcut: "/img/png/logo.png",
+    apple: "/img/png/logo_nomask.png",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/img/png/logo_nomask.png",
+    },
+  },
+  openGraph: {
+    siteName: "simbl",
+    images: "/img/png/logo.png",
+  },
+};
+
+export const viewport: Viewport = {
+  colorScheme: "dark",
+  themeColor: "#000000",
+  userScalable: false,
+};
 
 export default function RootLayout({
   children,
@@ -10,7 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Metadata />
       <body>
         <Header />
         {children}

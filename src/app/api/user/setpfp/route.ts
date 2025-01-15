0e21786415d91497bed5edc600a2c470d3be8f34
userbyte <42632711+userbyte@users.noreply.github.com>
2025-01-15 17:02:46 -0500

    enable setpfp route

--- src/app/api/user/setpfp/route.ts
+++ src/app/api/user/setpfp/route.ts
@@ -1,35 +1,35 @@
 import { NextResponse } from "next/server";
-// import path from "path";
-// import { writeFile } from "fs/promises";
-// import { GetSession } from "../../authv2";
+import path from "path";
+import { writeFile } from "fs/promises";
+import { GetSession } from "../../authv2";
 
 // POST /api/user/setpfp
-// export const POST = async (req, res) => {
-export const POST = async () => {
-  // TODO: make this work, i think it should kind of work but im disabling this because the build is failing on file.arrayBuffer()
-  return NextResponse.json({ Message: "currently disabled", status: 201 });
-  // const session = await GetSession();
-  // const username = session.name;
-  // const formData = await req.formData();
+export const POST = async (req: Request) => {
+  const session = await GetSession();
+  const username = session.user.name;
+  const formData = await req.formData();
 
-  // const file = formData.get("file");
-  // if (!file) {
-  //   return NextResponse.json({ error: "No files received." }, { status: 400 });
-  // }
-
-  // const buffer = Buffer.from(await file.arrayBuffer());
-  // // const filename = file.name.replaceAll(" ", "_");
-  // // filename is just the username
-  // const filename = username;
-  // console.log(filename);
-  // try {
-  //   await writeFile(
-  //     path.join(process.cwd(), "public/pfps/" + filename),
-  //     buffer
-  //   );
-  //   return NextResponse.json({ Message: "Success", status: 201 });
-  // } catch (error) {
-  //   console.log("Error occured ", error);
-  //   return NextResponse.json({ Message: "Failed", status: 500 });
-  // }
+  const file: FormDataEntryValue | File | null = formData.get("file");
+  // dumb typing bs, i have to accept that file may be a string here
+  if (!file || typeof file === "string") {
+    return NextResponse.json({ error: "no files received" }, { status: 400 });
+  }
+  const buffer = Buffer.from(await file.arrayBuffer());
+  // const filename = file.name.replaceAll(" ", "_");
+  // filename is just the username
+  const filename = username.replaceAll(" ", "_");
+  console.log("setting the pfp of:", username);
+  try {
+    await writeFile(
+      path.join(process.cwd(), "public/img/pfp/" + filename),
+      buffer
+    );
+    return NextResponse.json({ status: "success" }, { status: 201 });
+  } catch (error) {
+    console.log("/api/user/setpfp :: error:", error);
+    return NextResponse.json(
+      { status: "error", error: "internal server error :(" },
+      { status: 500 }
+    );
+  }
 };
